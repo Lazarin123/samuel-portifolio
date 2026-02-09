82e4fdb21f70aee6894d0df1ac767dd1d8818df4
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-02-09 15:18:34 -0300

    add project complete

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/samuel-portifolio/", 
+  resolve: {
+    alias: {
+      // Isso cria o atalho '@' que aponta para a pasta 'src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // IMPORTANTE: Use o alias '@' aqui para evitar erros de caminho relativo
+        additionalData: `
+          @use "sass:color";
+          @use "@/styles/abstracts/variables" as *;
+          @use "@/styles/abstracts/mixins" as * ;
+        `,
+      },
+    },
+  },
 })
